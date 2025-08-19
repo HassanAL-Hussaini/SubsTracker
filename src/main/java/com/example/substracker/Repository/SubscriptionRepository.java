cf28a15fb00009dadf906bcc0e4b05935dcef6e2
Blooming0 <hassanalhosyni@gmail.com>
2025-08-19 13:52:07 +0300

    Repository annotation

--- src/main/java/com/example/substracker/Repository/SubscriptionRepository.java
+++ src/main/java/com/example/substracker/Repository/SubscriptionRepository.java
@@ -2,7 +2,9 @@ package com.example.substracker.Repository;
 
 import com.example.substracker.Model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription,Integer> {
     Subscription findSubscriptionById(Integer id);
 }
