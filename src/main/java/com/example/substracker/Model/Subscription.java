018ee043656c131faf66a85977ffaf16fd6b2a8a
safcsp dev <dev@test.com>
2025-08-19 14:00:00 +0300

    add Subscription

--- src/main/java/com/example/substracker/Model/Subscription.java
+++ src/main/java/com/example/substracker/Model/Subscription.java
@@ -22,7 +22,7 @@ public class Subscription {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer subscriptionId;
 
     @NotEmpty(message = "Subscription name cannot be empty")
     @Size(max = 100, message = "Subscription name must not exceed 100 characters")
