b98dfb4fe466565aa166a93f687afc88df5a98c6
Blooming0 <hassanalhosyni@gmail.com>
2025-08-19 17:06:24 +0300

    Edited some Issues

--- src/main/java/com/example/substracker/Service/SpendingAnalysisService.java
+++ src/main/java/com/example/substracker/Service/SpendingAnalysisService.java
@@ -1,4 +1,27 @@
 package com.example.substracker.Service;
 
+import com.example.substracker.Model.SpendingAnalysis;
+import com.example.substracker.Model.Subscription;
+import com.example.substracker.Repository.SpendingAnalysisRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
 public class SpendingAnalysisService {
+    private final SpendingAnalysisRepository spendingAnalysisRepository;
+
+    public List<SpendingAnalysis> getAllSpendingAnalysis(){
+        return spendingAnalysisRepository.findAll();
+    }
+    //there is No Delete in spending Analysis
+
+    public void AddSpendingAnalysis(){
+
+    }
+
+
+
+
 }
